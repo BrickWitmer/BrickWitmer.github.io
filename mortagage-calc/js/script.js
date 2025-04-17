99b4a550643dbbb3f57cc70a394a84d671b7d8f1
Brick Witmer <witmer.b@stu.janesville.k12.wi.us>
2025-04-17 14:27:43 -0500

    4/17

--- mortagage-calc/js/script.js
+++ mortagage-calc/js/script.js
@@ -4,13 +4,12 @@ function calc() {
     let yea = Number(document.getElementById("years").value);
     let mon = Number(document.getElementById("month").value);
     let p = document.getElementById("out");
-    let n = 12
     let nt = 12 * yea
     let rn = rate/12
 
     let rem = prin * (1 + rn)**nt - mon * ((1+rn)**nt - 1) / rn;
 
-    p.innerHTML = "Reamaining loan balance: $" + rem.toFixed(2);
+    p.innerHTML = "Remaining loan balance: $" + rem.toFixed(2);
     document.getElementById('modal').showModal()
 }
 function reset() {
