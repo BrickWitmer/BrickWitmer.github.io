f3f67ec64efc4eb40aa24b383cb709ebd29e7b24
Brick Witmer <witmer.b@stu.janesville.k12.wi.us>
2025-05-21 14:39:13 -0500

    5/21

--- interactive.js
+++ interactive.js
@@ -1,5 +1,15 @@
-let c1 = document.getElementById("color1").value;
-let c2 = document.getElementById("color2").value;
-function changetheme(col1,col2){
-    
+let r = document.querySelector(':root')
+function changetheme(){
+    let c1 = document.getElementById("color1").value;
+    let c2 = document.getElementById("color2").value;
+    r.style.setProperty('--color1',c1);
+    r.style.setProperty('--color2',c2);
+    localStorage.setItem('--color1',c1);
+    localStorage.setItem('--color2',c2);
+}
+function reset(){
+    let c1 = '#222222';
+    let c2 = '#555555';
+    r.style.setProperty('--color1',c1);
+    r.style.setProperty('--color2',c2);
 }
